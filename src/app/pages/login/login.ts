c5cf3878d7af0f972c0e23322673fde03ffb8615
Muhammad-Affan-Malik <muhammadaffanawan707@gmail.com>
2025-10-05 00:52:36 +0500

    Added HTML and TS file ( LOGIN )

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface LoginData {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  loginData: LoginData = {
+    email: '',
+    password: '',
+    rememberMe: false
+  };
 
+  onSubmit() {
+    if (this.loginData.email && this.loginData.password) {
+      console.log('Login attempt:', this.loginData);
+      // Here you would typically call an authentication service
+      // For now, just log the data
+      alert(`Login attempt with email: ${this.loginData.email}`);
+    }
+  }
+
+  forgotPassword() {
+    console.log('Forgot password clicked');
+    // Here you would typically navigate to forgot password page
+    alert('Forgot password functionality would be implemented here');
+  }
+
+  goToRegister() {
+    console.log('Go to register clicked');
+    // Here you would typically navigate to register page
+    alert('Navigate to register page functionality would be implemented here');
+  }
 }
